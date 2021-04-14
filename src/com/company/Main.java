fbe01c677f3a27b4d694599fbd9051b5484e98dc
vova <volodymyrlakusta28@gmail.com>
2021-04-14 20:49:46 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        final String PATH = "src/com/company/logs2019.log";
+
+        String logs = new String(Files.readAllBytes(Paths.get(PATH)));
+        final String[] lines = logs.split("\n");
+
+        System.out.println(lines.length);
+        System.out.println("String.split-------------");
+        LocalDateTime start = LocalDateTime.now();
+        int counter = 0;
+        for (int i = 0; i < lines.length ; i++) {
+            if (lines[i].contains("ERROR")) counter++;
+        }
+        System.out.println("ERRORS  - " + counter);
+        LocalDateTime finish = LocalDateTime.now();
+        System.out.println(ChronoUnit.MILLIS.between( start, finish));
+
+        System.out.println("File.Lines-------------");
+
+        start = LocalDateTime.now();
+        System.out.println(Files.lines(Paths.get(PATH)).filter(e -> e.contains("ERROR")).count());
+        finish = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.MILLIS.between( start, finish));
+
+    }
+    /* Output:
+    433387
+    String.split-------------
+    ERRORS  - 105
+    151
+    File.Lines-------------
+    105
+    725
+     */
+
+}
